2c1bb9d63662924f38bf0bb4cb030d7ba0317d2c
PalladaV <aybol1b5c@yahoo.com>
2022-04-01 22:54:55 +0300

    add HW3

---
+++ HW3/Apple.java
@@ -0,0 +1,12 @@
+package HW3;
+
+public class Apple extends Fruit {
+    public Apple(float weight) {
+        super(1);
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(1);
+    }
+}
