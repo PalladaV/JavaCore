743e57d50b20f83937435159d102a7cc87336d96
PalladaV <aybol1b5c@yahoo.com>
2022-04-05 23:47:58 +0300

    add HW4

---
+++ HW4/PhoneBook.java
@@ -0,0 +1,28 @@
+package HW4;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class PhoneBook {
+    private HashMap<String, ArrayList<String>> phoneBook = new HashMap<>();
+
+    public void add(String lastName, String phoneNumber) {
+        ArrayList<String> phonesForLastName = phoneBook.getOrDefault(lastName, new ArrayList<>());
+        phonesForLastName.add(phoneNumber);
+        phoneBook.put(lastName, phonesForLastName);
+    }
+
+    public ArrayList<String> get(String lastName) {
+        return phoneBook.get(lastName);
+    }
+
+    public static void main(String[] args) {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Sidorov", "9896959392");
+        phoneBook.add("Sidorov1", "54321");
+        phoneBook.add("Sidorov2", "367589");
+        phoneBook.add("Sidorov", "11111111");
+
+        System.out.println(phoneBook.get("Sidorov5"));
+    }
+}
